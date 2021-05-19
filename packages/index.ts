e31e099e0c5832989b191be3a28265d459dd035a
hcg1023 <2293885211@qq.com>
2021-05-19 11:24:35 +0800

    feat: 修改抛出内容

--- packages/index.ts
+++ packages/index.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import 'vue/dist/vue.cjs.prod.js'
 // 人员卡片
-// import './ok-avatar'
-// import './ok-person-card'
+import './ok-avatar'
+import './ok-person-card'
 import './ok-person-cell'
-// import './ok-person-group'
+import './ok-person-group'
